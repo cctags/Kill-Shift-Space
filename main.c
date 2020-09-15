5d6fd6960e25eab73ab44b3df55f1f5892e51e86
ctags <sherking@gmail.com>
2020-09-15 09:55:18 +0800

    Fix build

--- main.c
+++ main.c
@@ -1,28 +1,24 @@
 /*
-*	A simple program testing Windows global hotkey usage. 
+*	A simple program testing Windows global hotkey usage.
 *	Using the keybind 'shift + space' will print a message in the program console.
 *	Largely taken from MSDN topic on RegisterHotkey here:
 *	https://msdn.microsoft.com/en-us/library/windows/desktop/ms646309%28v=vs.85%29.aspx
 */
 
-
+#include <stdio.h>
 #include <Windows.h>
-#include <iostream>
-
-using std::cout;
-using std::endl;
 
 // comment this will show the console
 #pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
 #define HOTKEY_ID 0x53C8
+#define MOD_NOREPEAT 0x4000
 
 int main(int argc, char *argv[])
 {
-
 	if (RegisterHotKey(NULL, HOTKEY_ID, MOD_SHIFT | MOD_NOREPEAT, 0x20))
 	{
-		cout << "Successfully registered shift + space global hotkey" << endl;
+		printf("Successfully registered shift + space global hotkey\n");
 	}
 
 	MSG winMessage = { 0 };
@@ -32,11 +28,11 @@ int main(int argc, char *argv[])
 	{
 		if (winMessage.message == WM_HOTKEY)
 		{
-			cout << "Received hotkey message" << endl;
-			
+			printf("Received hotkey message\n");
+
 			// "shift space" map to "space".
 			// {
-			keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0); 
+			keybd_event(VK_SHIFT, 0, KEYEVENTF_KEYUP, 0);
 			keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
 			keybd_event(VK_SPACE, 0, 0, 0);
 			keybd_event(VK_SPACE, 0, KEYEVENTF_KEYUP, 0);
@@ -47,7 +43,7 @@ int main(int argc, char *argv[])
 	// unregister the hotkey
 	if (UnregisterHotKey(NULL, HOTKEY_ID))
 	{
-		cout << "Successfully unregistered hotkey" << endl;
+		printf("Successfully unregistered hotkey\n");
 	}
 
 	return 0;
